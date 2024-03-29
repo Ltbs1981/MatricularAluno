5308c21e2b7cffe151eb95b4c41a5f73a3b7799e
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-29 00:54:37 -0300

    Início projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatricularAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
